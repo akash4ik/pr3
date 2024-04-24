089fc716e6278bc9e658b33080bec4b4966b014e
DebDAB <prrrr116@gmail.com>
2024-04-24 11:18:04 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace RP33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char op;
+            double first, second, result;
+
+            Console.Write("Введите первое число: ");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            second = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите оператор (+, -, *, /): ");
+            op = (char)Console.Read();
+
+            switch (op)
+            {
+                case '+':
+                    result = first + second;
+                    Console.WriteLine("{0} + {1} = {2}", first, second, result);
+                    break;
+
+                case '-':
+                    result = first - second;
+                    Console.WriteLine("{0} - {1} = {2}", first, second, result);
+                    break;
+
+                case '*':
+                    result = first * second;
+                    Console.WriteLine("{0} * {1} = {2}", first, second, result);
+                    break;
+
+                case '/':
+                    result = first / second;
+                    Console.WriteLine("{0} / {1} = {2}", first, second, result);
+                    break;
+
+                default:
+                    Console.WriteLine("Недопустимый оператор");
+                    break;
+            }
+        }
+    }
+}
